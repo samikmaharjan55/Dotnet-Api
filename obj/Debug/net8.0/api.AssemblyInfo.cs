7cb0d433f8fb0cb0688f6c64ab1f9d1682e6b6ea
samikmaharjan55 <samikmaharjan55@gmail.com>
2024-09-30 14:36:17 +0545

    Dtos

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ddbb6db05d987c522fdfe23d2b943fcd3209e78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8af137ab91a0944826256e7df07f425ceb5aab77")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
