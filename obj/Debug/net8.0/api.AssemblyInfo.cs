8af137ab91a0944826256e7df07f425ceb5aab77
samikmaharjan55 <samikmaharjan55@gmail.com>
2024-09-30 13:02:39 +0545

    Controllers

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a29b4c225ba6541f33359405621027aa63119226")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ddbb6db05d987c522fdfe23d2b943fcd3209e78")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
