0373f82c8280f915fde6fa211a609028ffeb2bfa
samikmaharjan55 <samikmaharjan55@gmail.com>
2024-10-03 17:20:39 +0545

    Filtering

---
+++ Helpers/QueryObject.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace api.Helpers
+{
+    public class QueryObject
+    {
+        public string? Symbol { get; set; } = null;
+        public string? CompanyName { get; set; } = null;
+    }
+}
