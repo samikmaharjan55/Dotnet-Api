27528d9ade001b76afdf73e27f11a2de22101eb0
samikmaharjan55 <samikmaharjan55@gmail.com>
2024-10-07 17:50:12 +0545

    Token Service

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4a86770f318451383bdb0b14b23084f668b2193")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f42e79bc51d465d5d7848831027b56b0b190aa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
