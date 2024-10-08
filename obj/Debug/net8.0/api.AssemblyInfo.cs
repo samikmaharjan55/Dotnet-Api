9d092d0c227818319c69c49af152622008bacfaa
samikmaharjan55 <samikmaharjan55@gmail.com>
2024-10-08 13:16:03 +0545

    Portfolio Get

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb46671b2fbf60a6a1c0b29b444effdfaa1da795")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c75961c1ba08cd7c7f0bd990bc9b3f83f90dca69")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
